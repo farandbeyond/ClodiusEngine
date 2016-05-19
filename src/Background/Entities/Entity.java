35146d0469281deb1e49507719b03f17c323fca2
farandbeyond <connorjh4@gmail.com>
2016-05-19 14:20:42 -0400

    File Structure
    just basics

---
+++ src/Background/Entities/Entity.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Background.Entities;
+
+/**
+ *
+ * @author Connor
+ */
+public class Entity {
+    String name;
+    public Entity(String name){
+        this.name = name;
+    }
+    public String getName(){return name;}
+}
