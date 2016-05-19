85eeaa1c9534836c0988baea95bc5da2fddd4ef1
farandbeyond <connorjh4@gmail.com>
2016-05-19 15:55:05 -0400

    Basic Entity
    Needs more work in constructor. ready for the moment

--- src/Background/Entities/Entity.java
+++ src/Background/Entities/Entity.java
@@ -5,26 +5,64 @@
  */
 package Background.Entities;
 
+import java.util.Random;
+
 /**
  *
  * @author Connor
  */
 public class Entity {
+    public static final int 
+            STAMINA=0, 
+            MAXSTAMINA=1, 
+            ENERGY=2, 
+            MAXENERGY=3, 
+            FORTITUDE = 4, 
+            CONSTITUTION=5, 
+            TECHNICAL = 6, 
+            KNOWLEDGE = 7, 
+            SWIFTNESS = 8;
     String name;
-    Stat Strength;
-    public Entity(String name, int strength){
+    Stat[] allStats;
+    public Entity(String name){
         this.name = name;
-        Strength = new Stat(strength,"Strength Stat, used for weapon damage");
     }
     public String getName(){return name;}
     private class Stat{
-        int value;
-        String description;
-        Stat(int value, String description){
-            this.value = value;
-            this.description = description;
+        int baseStat, currentStat;
+	int baseGrowth, randGrowth;
+        Random rand;
+        /**
+         * used during character creation
+         */
+        Stat(int base, int baseGrowth, int randGrowth){
+            rand = new Random();
+            baseStat = base;
+            currentStat = base;
+            this.baseGrowth = baseGrowth;
+            this.randGrowth = randGrowth;
         }
-        public int getStat(){return value;}
-        public String getDesc(){return description;}
+        /**
+         * used when loading a game
+         */
+        Stat(int base, int bGrowth, int rGrowth, int currentStat){
+            baseStat = base;
+            baseGrowth = bGrowth;
+            randGrowth = rGrowth;
+            this.currentStat = currentStat;
+            rand = new Random();
+        }
+        //gets
+        public int getBaseStat(){return baseStat;}
+        public int getCurrentStat(){return currentStat;}
+        public int getBaseGrowth(){return baseGrowth;}
+        public int getRandGrowth(){return randGrowth;}
+        //sets and stat controlling
+        public void levelUp(){
+            baseStat+=baseGrowth+rand.nextInt(randGrowth);
+        }
+        public void setStat(int newStat){baseStat = newStat;}
+        public void increaseStat(int increase){currentStat+=increase;}
+        public void decreaseStat(int decrease){currentStat-=decrease;}
     }
 }
